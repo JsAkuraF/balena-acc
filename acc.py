3a24e0a534cb7643ea70eb8cf6667975bb4f53c7
JsAkuraF <julia_abril@hotmail.es>
2018-10-20 16:04:07 +0200

    acc.py editado

--- acc.py
+++ acc.py
@@ -13,4 +13,4 @@ while True:
 	z=round(z, 0)
 	message = 'Temperature is %d F Humidity is %d percent Pressure is %d mbars' %(x,y,z)
 
-	sense.show_message(message, scroll_speed=(0.00),text_colour=[200,0,200], back_colour= [0,0,200])
+	sense.show_message(message, scroll_speed=(0.00),text_colour=[200,0,200], back_colour= [0,0,0])
