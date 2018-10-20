16a88b7448f921a482b646a194432abe5349056c
JsAkuraF <julia_abril@hotmail.es>
2018-10-20 16:17:50 +0200

    acc.py editado

--- acc.py
+++ acc.py
@@ -7,11 +7,11 @@ while True:
 	x = acceleration['x']
 	y = acceleration['y']
 	z = acceleration['z']
-
-	x=round(x, 0)
-	y=round(y, 0)
-	z=round(z, 0)
-	message = '%d, %d, %d' %(x,y,z)
+#he anulado los round para obtener decimales,   he cambiado las d por f y he puesto 0.1 para obtener un decimal.
+	#x=round(x, 0)
+	#y=round(y, 0)
+	#z=round(z, 0)
+	message = '%.1f, %.1f, %.1f' %(x,y,z)
 
 	sense.show_message(message, scroll_speed=(0.05),text_colour=[200,0,200], back_colour= [0,0,0])
 	time.sleep(1)
